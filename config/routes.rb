ef592bbac23d00ba6f2924d9f0810be08123d9ca
Lienchi <Lienchi.Lai@gmail.com>
2017-12-14 22:07:10 +0800

    rails generate controller categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show]
-  root "restaurants#index"
   resources :categories, only: :show
+  root "restaurants#index"
 
   namespace :admin do
     resources :restaurants
