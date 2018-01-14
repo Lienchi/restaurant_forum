0bb1f8c3bb681610fae53d8dfb8c04bd3da27c50
Lienchi <Lienchi.Lai@gmail.com>
2018-01-14 21:10:10 +0800

    user can upload avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarImageUploader
+
   has_many :comments
+  
   def admin?
     self.role == "admin"
   end
