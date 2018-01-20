9cb7a9f2426a1fa1328f32af56e22318a65a4bfb
Lienchi <Lienchi.Lai@gmail.com>
2018-01-20 14:03:19 +0800

    add like/unlike action and button

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,9 +9,13 @@ class Restaurant < ApplicationRecord
   has_many :favorited_users, through: :favorites, source: :user
 
   has_many :likes, dependent: :destroy
-  has_many :liked_users, through: :likes, source :user
+  has_many :liked_users, through: :likes, source: :user
 
   def is_favorited?(user)
     self.favorited_users.include?(user)
+  end
+
+  def is_liked?(user)
+    self.liked_users.include?(user)
   end
 end
